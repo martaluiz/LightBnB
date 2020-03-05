657ee43d479bbef25060bbf9c839fceeeeebaea3
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-03-05 21:20:48 +0000

    adding queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,19 +16,25 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
+const getUserWithEmail = (email) => {
+  return pool.query(`
+    SELECT * FROM users
+    WHERE email = $1;
+  `, [email])
+  .then(res => {
+    console.log(res.rows);
+    if (res.rows) {
+      return res.rows;
     } else {
-      user = null;
+      return null;
     }
-  }
-  return Promise.resolve(user);
+  })
+  .catch(err => {
+    console.log(`Error:`, err);
+  });
 }
 exports.getUserWithEmail = getUserWithEmail;
+
 
 /**
  * Get a single user from the database given their id.
@@ -36,8 +42,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = (id) => {
+  return pool.query(`
+    SELECT * FROM users
+    WHERE id = $1;
+  `, [id])
+  .then(res => {
+    if (res.rows) {
+      return res.rows[0];
+    } else {
+      return null;
+    }
+  });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -46,11 +62,20 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser =  (userObj) => {
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `, [userObj.name, userObj.email, userObj.password])
+  .then(res => {
+    console.log(`new user: `, res.rows[0]);
+    if (res.rows) {
+      return res.rows[0];
+    } else {
+      return null;
+    }
+  })
 }
 exports.addUser = addUser;
 
